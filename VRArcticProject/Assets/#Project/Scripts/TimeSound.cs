fac782701e6f1fefba6b775cd7b6949b96a63b44
Clorinthe <43383448+Clorinthe@users.noreply.github.com>
2020-09-04 13:08:22 +0200

    les sons fonctione

---
+++ VRArcticProject/Assets/#Project/Scripts/TimeSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSound : MonoBehaviour
+{
+    AudioSource sonCris;
+    float currentTime;
+    public float startTime = 10f;
+    bool sonJouer = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sonCris = GetComponent<AudioSource>();
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        Debug.Log(currentTime);
+        if (sonJouer == false)
+        {
+            if (currentTime < 0)
+            {
+                sonCris.Play();
+                Debug.Log("Son lancer !");
+                sonJouer = true;
+
+            }
+
+        }
+    }
+}
