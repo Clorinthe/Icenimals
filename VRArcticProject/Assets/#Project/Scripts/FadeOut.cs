82e1f591fe9ecd6c596d245320788ad4be179a6c
Clorinthe <43383448+Clorinthe@users.noreply.github.com>
2020-09-10 16:53:24 +0200

    splash screen

---
+++ VRArcticProject/Assets/#Project/Scripts/FadeOut.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.SetBool("fadeout", true);
+
+    }
+
+}
---
+++ VRArcticProject/Assets/#Project/Scripts/TimingSplash.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimingSplash : MonoBehaviour
+{
+    public Animator anim;
+    float currentTime;
+    float currentTime2;
+    public float startTime = 2f;
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyRootOnload(gameObject);
+        currentTime = startTime;
+        currentTime2 = 1f;
+    }
+    void DontDestroyRootOnload(GameObject obj)
+    {
+        DontDestroyOnLoad(obj.transform.root.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        Debug.Log(currentTime);
+        if (currentTime < 0)
+        {
+
+            anim.SetBool("fadein", true);
+            currentTime2 -= 1 * Time.deltaTime;
+            if (currentTime2 < 0)
+            {
+                SceneManager.LoadScene(sceneName);
+                
+            }
+
+
+
+
+        }
+    }
+}
